8d3a1d6b50f6a99d4091347f1b9a6408bfc1254d
EnriqueDoster <12348747+EnriqueDoster@users.noreply.github.com>
2018-09-17 10:39:33 -0600

    Update SLURM_task_launcher.sh

--- SLURM_task_launcher.sh
+++ SLURM_task_launcher.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 #SBATCH --partition=shas
-# Number of tasks needed for use case (example):
 #SBATCH --ntasks=1
-#
-# Processors per task:
 #SBATCH --qos=normal
 #SBATCH --cpus-per-task=1
 #SBATCH --time=20:00:00
@@ -11,11 +8,9 @@
 #SBATCH --mail-user=enriquedoster@gmail.com
 #SBATCH --mail-type=ALL
 
-### Use the multiple program flag to specify command file job
-#srun --multi-prog assembly.tasks
-
-for FILE in assembly/R*.sh; do
+for FILE in assembly/*.sh; do
 echo ${FILE}
 sbatch ${FILE} -o ${FILE}.stdout.txt -e ${FILE}.stderr.txt
-sleep 1 # pause to be kind to the scheduler
+sleep 5m # pause to be kind to the scheduler
 done
+

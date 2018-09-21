fe04ba68c55a337eca1209c2b977c4862a3df710
EnriqueDoster <12348747+EnriqueDoster@users.noreply.github.com>
2018-09-21 13:11:53 -0600

    Update parallel_assembly.bash

--- parallel_assembly.bash
+++ parallel_assembly.bash
@@ -2,7 +2,6 @@
 # Run this script to make the various SBATCH files needed for al your samples
 # Use: parallel -j 1 "../parallel_assembly.bash {}" ::: input_dir/*R1.fastq.gz
 out_directory="assembly"
-input_dir
 
 samplename=$( basename $1 | sed -r 's/R1_001.fastq.gz//' )
 input_dir=$(dirname $1)

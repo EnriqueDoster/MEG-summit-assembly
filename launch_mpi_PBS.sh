3f58c09d8fed3521010e56f5cc65c3b8524e6eb6
EnriqueDoster <12348747+EnriqueDoster@users.noreply.github.com>
2019-05-03 09:44:41 -0600

    Add files via upload

--- launch_mpi_PBS.sh
+++ launch_mpi_PBS.sh
@@ -10,7 +10,7 @@ module load java/jdk1.8.0_144
 module load singularity/current
 module load ompi/gnu.mesabi
 
-mpirun --pernode ./nextflow run main_amrplusplus_snp.nf -resume -profile slurm \
+mpirun --pernode ./nextflow run main_amrplusplus_snp.nf -resume -profile msi_pbs \
 -w /home/noyes046/slizo001/projects/cipro/work --threads 15 \
 --output /home/noyes046/slizo001/projects/cipro/cipro_amrplusplus --host /home/noyes046/slizo001/projects/HostGenome/GRCh38_latest_genomic.fna.gz \
 --reads "/home/noyes046/slizo001/projects/cipro/data/*_{1,2}.fastq.gz" -with-mpi
